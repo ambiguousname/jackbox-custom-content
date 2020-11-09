97148f3abcb6ee3775cf7c7135315f79fd131320
ambiguousname <tyly04@gmail.com>
2020-11-08 16:18:19 -0800

    Fixed weird menu option.

--- jppc.py
+++ jppc.py
@@ -310,7 +310,7 @@ quiplash_3 = SelectionWindow("Quiplash 3 Content Selection", ["Please select the
 }, "create_content")
 
 #Main Menu stuff
-create_content = SelectionWindow("Select a game", ["Select a game.", ("Quiplash 3"), "game"],{
+create_content = SelectionWindow("Select a game", ["Select a game.", (["Quiplash 3"]), "game"],{
     "Blather Round": None,
     "Devils and the Details": None,
     "Talking Points": None,
