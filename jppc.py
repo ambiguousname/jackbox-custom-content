ff7d6faacfcc1459c1f80eba943d8b01b2453ba8
ambiguousname <tyly04@gmail.com>
2020-12-01 17:01:36 -0800

    Fixed a champ'd up thing

--- jppc.py
+++ jppc.py
@@ -6,7 +6,7 @@ from shutil import copyfile, rmtree
 #TODO:
 # Test importing content when you already have existing content
 # Test importing content with non-existent custom files (like a .JPG or .OGG)
-# Test Champ'd Up content
+# Test Champ'd Up content, Quiplash 3 Content (especially with using only pre-existing content.)
 # Edit content by game?
 
 def id_gen(values): #id_gen needs a values dict to work with
@@ -367,7 +367,7 @@ def game_content_del(game, content_type):
 
 def del_all_else(selected=None):
     layout = [[sg.Text("Are you absolutely sure you want to do this?")], [sg.Text("This option will effectively delete all the game's content files so that you can only play with your own custom content. Please make sure you have backups.")],
-    [sg.Text("Please select the game(s) whose content you'd like to remove: "), sg.Listbox(("Quiplash3", "JackboxTalks"), size=(50, 4), key="game_choice", select_mode=sg.LISTBOX_SELECT_MODE_MULTIPLE)], [sg.Checkbox("I am absolutely sure I want to do this. Please delete all non-custom content.", key="sure")], [sg.Button("Ok"), sg.Button("Go Back")]]
+    [sg.Text("Please select the game(s) whose content you'd like to remove: "), sg.Listbox(("JackboxTalks", "Quiplash3", "Champ'd Up"), size=(50, 4), key="game_choice", select_mode=sg.LISTBOX_SELECT_MODE_MULTIPLE)], [sg.Checkbox("I am absolutely sure I want to do this. Please delete all non-custom content.", key="sure")], [sg.Button("Ok"), sg.Button("Go Back")]]
     window = sg.Window("Delete all non-custom content", layout)
     while True:
         event, values = window.read()
