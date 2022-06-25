2b0646667d8659447b3e284117605e3d1142b66f
ambiguousname <tyly04@gmail.com>
2022-06-24 20:56:34 -0700

    Switched to fltk for simpler GUI

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,12 @@
-use iced::{button, Button, Column, Element, Text, Settings, Sandbox};
+use fltk::{app, button::Button, frame::Frame, prelude::*, window::Window};
 
-#[derive(Default)]
-struct MainWindow {
-    select_button: button::State,
-}
-
-#[derive(Debug, Clone, Copy)]
-pub enum Message {
-    SelectPressed,
-}
-
-impl Sandbox for MainWindow {
-    type Message = Message;
-
-    fn new() -> Self {
-        Self::default()
-    }
-
-    fn title(&self) -> String {
-        String::from("Jackbox Custom Content")
-    }
-
-    fn view(&mut self) -> Element<Message> {
-        Column::new()
-        .push(
-            Button::new(&mut self.select_button, Text::new("Select"))
-            .on_press(Message::SelectPressed)
-        ).into()
-    }
-
-    fn update(&mut self, message: Message) {
-        match message {
-            Message::SelectPressed => {
-                println!("Select pressed.");
-            }
-        }
-    }
-}
-
-fn main() -> iced::Result {
-    println!("Starting window...");
-    MainWindow::run(Settings::default())
+fn main() {
+    let app = app::App::default();
+    let mut wind = Window::new(100, 100, 400, 300, "Hello from rust");
+    let mut frame = Frame::new(0, 0, 400, 200, "");
+    let mut but = Button::new(160, 210, 80, 40, "Click me!");
+    wind.end();
+    wind.show();
+    but.set_callback(move |_| frame.set_label("Hello World!")); // the closure capture is mutable borrow to our button
+    app.run().unwrap();
 }
