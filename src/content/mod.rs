d797b258ac6f66706c5da6b8d511532370ce220c
Tyler K <tyly04@gmail.com>
2023-12-31 06:43:48 -0800

    Drafting content window setup

--- src/content/mod.rs
+++ src/content/mod.rs
@@ -21,7 +21,7 @@ mod imp {
         pub window_path : OnceCell<String>,
 
         #[property(get, set)]
-        pub window : OnceCell<gtk::Window>,
+        pub window : OnceCell<ContentWindow>,
     }
 
     #[glib::object_subclass]
@@ -53,46 +53,48 @@ impl Content {
     }
 }
 
-// Easier than doing the subclassing route for now:
-// pub trait ContentWindow {
-//     fn ensure_all_types();
-//     // Would be nice to figure this out somehow:
-//     fn create_content(&self);
-// }
+// Because GTK's implementation in Rust is a nightmare to read (I don't know why you would migrate an object oriented framework to something like Rust), this is the solution I've come up with to try and ensure some kind of consistency across different windows:
+mod content_window_imp {
+    use std::cell::RefCell;
 
-// mod content_window_imp {
-//     use super::*;
+    use gtk::glib::{subclass::Signal, once_cell::sync::Lazy};
 
-//     #[derive(Default)]
-//     pub struct ContentWindow {
+    use super::*;
 
-//     }
+    #[derive(Default, Properties)]
+    #[properties(wrapper_type=super::ContentWindow)]
+    pub struct ContentWindow {
+        #[property(get, set)]
+        pub content : RefCell<String>,
+    }
 
-//     #[glib::object_subclass]
-//     impl ObjectSubclass for ContentWindow {
-//         const NAME: &'static str = "JCCContentWindow";
-//         type Type = super::ContentWindow;
-//         type ParentType = gtk::Window;
-//     }
+    #[glib::object_subclass]
+    impl ObjectSubclass for ContentWindow {
+        const NAME: &'static str = "JCCContentWindow";
+        type Type = super::ContentWindow;
+        type ParentType = gtk::Window;
+    }
 
-//     impl ObjectImpl for ContentWindow {}
-//     impl WidgetImpl for ContentWindow {}
-//     impl WindowImpl for ContentWindow {}
-// }
+    #[derived_properties]
+    impl ObjectImpl for ContentWindow {
+        fn signals() -> &'static [Signal] {
+            static SIGNALS: Lazy<Vec<Signal>> = Lazy::new(|| {
+                vec![Signal::builder("create-finished").build()]
+            });
+            SIGNALS.as_ref()
+        }
+    }
+    impl WidgetImpl for ContentWindow {}
+    impl WindowImpl for ContentWindow {}
+}
 
-// glib::wrapper! {
-//     pub struct ContentWindow(ObjectSubclass<content_window_imp::ContentWindow>) @extends gtk::Window, gtk::Widget, @implements gtk::Accessible, gtk::Buildable, gtk::ConstraintTarget, gtk::Native, gtk::Root, gtk::ShortcutManager;
-// }
+glib::wrapper! {
+    pub struct ContentWindow(ObjectSubclass<content_window_imp::ContentWindow>) @extends gtk::Window, gtk::Widget, @implements gtk::Accessible, gtk::Buildable, gtk::ConstraintTarget, gtk::Native, gtk::Root, gtk::ShortcutManager;
+}
 
-// // impl ContentWindowExt for ContentWindow {
-// //     fn ensure_all_types() {
-        
-// //     }
-// // }
+// What would really be ideal is a way to set up some sort of trait that I can just implement or test for and be able to call like that.
+// Ideal might be to replace this with an interface (but that's even harder for me to figure out).
+// So this is what I'm stuck with for now.
+pub trait ContentWindowImpl : WindowImpl {}
 
-// pub trait ContentWindowImpl : WindowImpl {
-//     fn ensure_all_types();
-//     fn create_content(&self);
-// }
-
-// unsafe impl<T: ContentWindowImpl> IsSubclassable<T> for ContentWindow {}
+unsafe impl<T: ContentWindowImpl> IsSubclassable<T> for ContentWindow {}
--- src/content/quiplash3/prompts/mod.rs
+++ src/content/quiplash3/prompts/mod.rs
@@ -1,26 +1,29 @@
-use crate::{quick_template, content::Content};
+use crate::{quick_template, content::{ContentWindow, Content, ContentWindowImpl}};
 
 mod prompt_util;
 use prompt_util::QuiplashGenericRoundPrompt;
 
 // TODO: Transfer prompt data across notebooks?
-quick_template!(QuiplashRoundPrompt, "/content/quiplash3/prompts/round_prompt.ui", gtk::Window, (gtk::Widget, Content), (gtk::Native, gtk::Root, gtk::ShortcutManager));
+quick_template!(QuiplashRoundPrompt, "/content/quiplash3/prompts/round_prompt.ui", ContentWindow, (gtk::Window, gtk::Widget, Content), (gtk::Native, gtk::Root, gtk::ShortcutManager));
 impl ObjectImpl for imp::QuiplashRoundPrompt {}
 impl WidgetImpl for imp::QuiplashRoundPrompt {}
 impl WindowImpl for imp::QuiplashRoundPrompt {}
 
-// impl ContentWindowImpl for imp::QuiplashRoundPrompt {
-//     fn ensure_all_types() {
-//         QuiplashGenericRoundPrompt::ensure_all_types();
-//         QuiplashRoundPrompt::ensure_type();
-//     }
+impl ContentWindowImpl for imp::QuiplashRoundPrompt {
+    // fn ensure_all_types() {
+    //     QuiplashGenericRoundPrompt::ensure_all_types();
+    //     QuiplashRoundPrompt::ensure_type();
+    // }
 
-//     fn create_content(&self) {
+    // fn create_content(&self) {
         
-//     }
-// }
+    // }
+}
 
 impl QuiplashRoundPrompt {
+    // fn test(&self) {
+    //     self.test()
+    // }
     pub fn ensure_all_types() {
         QuiplashGenericRoundPrompt::ensure_all_types();
         QuiplashRoundPrompt::ensure_type();
