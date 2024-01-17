296f08087e3d77e00e6e70d757339aa32ff9f09a
Tyler K <tyly04@gmail.com>
2024-01-17 11:21:04 -0800

    Fixed binding bug for LabelledBox

--- src/templates/content_util/labelled_box.rs
+++ src/templates/content_util/labelled_box.rs
@@ -11,8 +11,8 @@ mod imp {
 	#[template(resource="/templates/content_util/labelled_box.ui")]
 	#[properties(wrapper_type=super::LabelledBox)]
 	pub struct LabelledBox {
-		#[template_child]
-		pub label_child : TemplateChild<gtk::Label>,
+		// #[template_child]
+		// pub label_child : TemplateChild<gtk::Label>,
 		// #[template_child]
 		// pub entry_child : TemplateChild<gtk::Entry>,
 
@@ -30,6 +30,14 @@ mod imp {
 		type Type = super::LabelledBox;
 		type ParentType = gtk::Box;
 		type Interfaces = (FormObject,);
+
+		fn class_init(klass: &mut Self::Class) {
+			klass.bind_template();
+		}
+	
+		fn instance_init(obj: &glib::subclass::InitializingObject<Self>) {
+			obj.init_template();
+		}
 	}
 }
 
