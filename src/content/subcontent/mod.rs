6e54cd353af461b701db8f3315f9c8a245ceb334
Tyler K <tyly04@gmail.com>
2024-03-01 12:09:11 -0800

    Figuring out the upcoming structure

--- src/content/subcontent/mod.rs
+++ src/content/subcontent/mod.rs
@@ -2,6 +2,11 @@ pub mod manifest;
 pub mod subfolder;
 pub mod content_folder;
 
+// TODO: How does this work for:
+// Making new content
+// Editing
+// Loading
+
 /// If we have a game folder (e.g., Quiplash3), there's chunks that belong to each [`super::ContentWindow`].
 /// [`Subcontent`] is a way to organize each chunk. They indicate how to load and write the chunk.
 /// Each [`Subcontent`] represents one file or folder, but it doesn't have to represent everything that it contains.
