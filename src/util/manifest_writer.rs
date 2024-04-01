730596993a44877c11d6d26a7161cb80b33ebb9f
Tyler K <tyly04@gmail.com>
2024-04-01 13:41:46 -0700

    Revert "Revert "Finally figured out how to claim ownership of Chars<'_>""
    This reverts commit 1cf63d01eac0fc62d8191cbc2e60cd787f5410b2.

--- src/util/manifest_writer.rs
+++ src/util/manifest_writer.rs
@@ -1,28 +1,26 @@
-use std::{fs::{File, OpenOptions}, io::{BufRead, BufReader, BufWriter, Error, Read}, path::Path, str::Chars};
+use std::{fs::{File, OpenOptions}, io::{BufRead, BufReader, BufWriter, Error, Read}, path::Path, rc::Rc, vec::IntoIter};
 
-struct CharFileIter<'a> {
-	// FIXME: The chars iterator needs to last as long as the line does.
-	// I think they need to be grouped into their own struct for that purpose.
-	curr_char : Option<Chars<'a>>,
-	line : String,
+struct CharFileIter {
+	// From https://stackoverflow.com/questions/47193584/is-there-an-owned-version-of-stringchars
+	line : Option<IntoIter<char>>,
 	reader : BufReader<File>,
 }
 
-impl<'a> CharFileIter<'a> {
-	fn get_line(&mut self) -> Option<<CharFileIter<'a> as Iterator>::Item> {
-		self.line = String::new();
+impl CharFileIter {
+	fn get_line(&mut self) -> Option<<CharFileIter as Iterator>::Item> {
+		let mut line = String::new();
 
-		let line_read = self.reader.read_line(&mut self.line);
+		let line_read = self.reader.read_line(&mut line);
 		if line_read.is_err() {
 			return Some(Err(line_read.err().unwrap()));
 		}
 		let bytes_read = line_read.unwrap();
 		
 		if bytes_read > 0 {
-			self.curr_char = Some(self.line.chars());
+			self.line = Some(line.chars().collect::<Vec<_>>().into_iter());
 
-			let chars = self.curr_char.as_mut();
-			let char = chars.unwrap().next();
+			let chars = self.line.as_mut().unwrap();
+			let char = chars.next();
 			return Some(Ok(char.unwrap()));
 		} else {
 			return None;
@@ -30,22 +28,15 @@ impl<'a> CharFileIter<'a> {
 	}
 }
 
-pub struct ManifestWriter<'a> {
-	read_iter : CharFileIter<'a>,
-	read_path : &'a Path,
-
-	writer : BufWriter<File>,
-}
-
-impl<'a> Iterator for CharFileIter<'a> {
+impl<'a> Iterator for CharFileIter {
 	type Item = std::io::Result<char>;
 
 	fn next(&mut self) -> Option<Self::Item> {
-		if self.curr_char.is_none() {
+		if self.line.is_none() {
 			return self.get_line();
 		}
 
-		let chars = self.curr_char.as_mut().unwrap();
+		let chars = self.line.as_mut().unwrap();
 
 		let next_char = chars.next();
 		if next_char.is_none() {
@@ -54,6 +45,14 @@ impl<'a> Iterator for CharFileIter<'a> {
 			Some(Ok(next_char.unwrap()))
 		}
 	}
+}
+
+
+pub struct ManifestWriter<'a> {
+	read_iter : CharFileIter,
+	read_path : &'a Path,
+
+	writer : BufWriter<File>,
 }
 
 impl<'a> ManifestWriter<'a> {
@@ -65,8 +64,7 @@ impl<'a> ManifestWriter<'a> {
 		Ok(ManifestWriter {
 			read_path: path,
 			read_iter: CharFileIter {
-				curr_char: None,
-				line: String::new(),
+				line: None,
 				reader: BufReader::new(read),
 			},
 			writer: BufWriter::new(write),
