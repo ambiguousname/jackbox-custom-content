5226b361b6817b8b8b7e5392abf1bcfebb04ab7e
Tyler K <tyly04@gmail.com>
2024-09-16 21:28:09 -0700

    Starting on serialization for struct

--- src/mod_manager/content_data.rs
+++ src/mod_manager/content_data.rs
@@ -7,38 +7,49 @@ use gtk::prelude::*;
 use gtk::subclass::prelude::*;
 
 use glib::Object;
+use serde::ser::SerializeStruct;
+use serde::{Deserialize, Serialize, Serializer};
 
 use crate::content::SubcontentBox;
 
 mod imp {
     use super::*;
 
-    #[derive(Default, Properties)]
+    /// Data for how to write a given [`crate::content::Content`] type to disk.
+    /// Serialized mostly for `manifest.json` that [`crate::mod_manager::mod_store::ModStore`] writes to.
+    #[derive(Default, Serialize, Properties)]
     #[properties(wrapper_type=super::ContentData)]
     pub struct ContentData {
         #[property(get, set)]
+        #[serde(serialize_with = "get_ref")]
         pub enabled: RefCell<bool>,
 
         /// The ID for this particular piece of content.
         #[property(get, set)]
+        #[serde(skip_serializing)]
         pub full_id: RefCell<String>,
 
         /// The number for this piece of content.
         #[property(get, set)]
+        #[serde(skip_serializing)]
         pub id: RefCell<u32>,
 
         #[property(get, set)]
+        #[serde(skip_serializing)]
         /// The relative path where this content is stored.
         pub relative_path: RefCell<PathBuf>,
 
         /// The particular type of this content, set in the xml definition for a ContentWindow.
         #[property(get, set)]
+        #[serde(skip_serializing)]
         pub content_type: RefCell<String>,
 
         /// Store for [`crate::content::Subcontent`], used to invoke various Subcontent functions for writing to and loading from disk.
+        #[serde(skip_serializing)]
         pub subcontent: RefCell<Vec<SubcontentBox>>,
 
         /// The arguments used when calling `subcontent` functions.
+        #[serde(skip_serializing)]
         pub subcontent_args: RefCell<Vec<Vec<&'static str>>>,
     }
 
@@ -50,6 +61,15 @@ mod imp {
 
     #[glib::derived_properties]
     impl ObjectImpl for ContentData {}
+}
+
+fn get_ref<T : Copy, S : Serializer>(t : RefCell<T>, serializer : S) -> Result<S::Ok, S::Error> {
+    ref_serialize(t.borrow().clone(), serializer)
+}
+
+// TODO: needs to be a trait
+fn ref_serialize<S: Serializer>(v : String, serializer: S) -> Result<S::Ok, S::Error> {
+    serializer.serialize_str(v.as_str())
 }
 
 glib::wrapper! {
